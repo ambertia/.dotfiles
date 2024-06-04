f7e52b55f8b89280527d02b06d0ac173eab60bfa
ambertia <95402631+ambertia@users.noreply.github.com>
2024-06-04 10:36:19 -0400

    Changed images to be stored in repo as list of URLs in attempt to mitigate apparent file corruption

---
+++ .backgrounds/generate-backgrounds.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Amber's Dotfiles
+
+# This script is meant to take all of the original downloaded images and
+# crop them to a specific aspect ratio using imagemagick. PNG's get
+# repaged, JPG's do not. Aspect ratio can be modified using the variable
+# below, although I designed this mainly for my laptop where there will
+# only be one screen with this specific aspect ratio 99.9% of the time.
+
+ASPECT_RATIO = "3:2"
+(cd originals && mogrify -gravity center -crop $ASPECT_RATIO +repage -path ../ *.png)
+(cd originals && mogrify -gravity center -crop $ASPECT_RATIO -path ../ *.jpg)
--- sddm/scripts/copyRandomWallpaper.sh
+++ sddm/scripts/copyRandomWallpaper.sh
@@ -4,7 +4,9 @@
 SDDM_CORNERS_FOLDER="/usr/share/sddm/themes/corners"
 
 # Get a random image file from the theme's background images folder (symlink to $USER_HOME/.backgrounds)
-IMAGE_FILE=$(find -L $SDDM_CORNERS_FOLDER/backgrounds -type f | shuf -n 1)
+IMAGE_FILE=$(find -L $SDDM_CORNERS_FOLDER/backgrounds -type f \( -name '*.png' -o -name '*.jpg' \) | shuf -n 1)
 
-# Copy a random wallpaper - SDDM can't use symlink to my home folder when I'm not logged in
+# Copy a random wallpaper - SDDM can't seem to actually use symlink to my home folder when I'm not logged in
+# The script is executed with superuser privilege and can therefore *copy* the image, but SDDM itself can't
+# access the original file in the user directory
 cp -f "$IMAGE_FILE" "$SDDM_CORNERS_FOLDER/randomlySelectedWallpaper"
