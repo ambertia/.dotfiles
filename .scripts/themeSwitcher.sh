9490ea6314b6ef244197608fca56414ff671c47c
ambertia <95402631+ambertia@users.noreply.github.com>
2024-10-19 12:11:08 -0700

    🩹 Move waybar management to end of theme script

--- .scripts/themeSwitcher.sh
+++ .scripts/themeSwitcher.sh
@@ -6,9 +6,6 @@ cp $1 ~/.backgrounds/current-wallpaper.png
 swww img $1
 # Generate palette
 wal -i $1 --cols16 -n --contrast 2 -a 50
-# Prevent duplicate waybar instances from modifications to hyprland.conf colors
-killall waybar
-waybar &
 # Import palette into Gradience
 gradience-cli import -p ~/.cache/wal/pywal.json
 # Apply Gradience pallete to GTK theme
@@ -17,3 +14,7 @@ gradience-cli apply -n pywal
 ~/.scripts/update-mako-theme.sh
 # Send a notification
 notify-send "Theme updated"
+# Prevent duplicate waybar instances from modifications to hyprland.conf colors
+killall waybar
+waybar &
+
