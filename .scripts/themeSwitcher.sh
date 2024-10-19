b43358bee931dfbb2a7ef0f547bd0029ac6f15ca
ambertia <95402631+ambertia@users.noreply.github.com>
2024-10-19 08:13:50 -0700

    🎨 Delete waybar launch.sh
    Delete waybar launch script and replace in the two locations it would otherwise be used with a 1-liner

--- .scripts/themeSwitcher.sh
+++ .scripts/themeSwitcher.sh
@@ -6,6 +6,9 @@ cp $1 ~/.backgrounds/current-wallpaper.png
 swww img $1
 # Generate palette
 wal -i $1 --cols16 -n --contrast 3 -a 50
+# Prevent duplicate waybar instances from modifications to hyprland.conf colors
+killall waybar
+waybar &
 # Import palette into Gradience
 gradience-cli import -p ~/.cache/wal/pywal.json
 # Apply Gradience pallete to GTK theme
