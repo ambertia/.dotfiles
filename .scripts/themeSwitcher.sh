47d665a394cb373844cd57c329ef0e0c5cfcb279
ambertia <95402631+ambertia@users.noreply.github.com>
2024-10-29 09:29:22 -0700

    🎨 Use image path in theme script as a variable

--- .scripts/themeSwitcher.sh
+++ .scripts/themeSwitcher.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+BACKGROUND_PATH=${1}
+echo $BACKGROUND_PATH
+
 # Update current-wallpaper
-cp $1 ~/.backgrounds/current-wallpaper
+cp $BACKGROUND_PATH $HOME/.backgrounds/current-wallpaper
 # Change background
-swww img $1
+swww img $BACKGROUND_PATH
 # Generate palette
-wal -i $1 --cols16 -n --contrast 2 -a 50
+wal -i $BACKGROUND_PATH --cols16 -n --contrast 2 -a 50
 # Import palette into Gradience
 gradience-cli import -p ~/.cache/wal/pywal.json
 # Apply Gradience pallete to GTK theme
@@ -15,6 +18,5 @@ gradience-cli apply -n pywal
 # Send a notification
 notify-send "Theme updated"
 # Prevent duplicate waybar instances from modifications to hyprland.conf colors
-killall waybar
-waybar &
+killall waybar && waybar &
 
