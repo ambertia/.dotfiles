cc50b1a724630e55ab1217b90d31bc8410d34309
ambertia <95402631+ambertia@users.noreply.github.com>
2024-12-08 17:23:24 -0800

    💄 Waybar custom/power colorizing and caching based on battery state

--- .scripts/waybar-battery-discharge.sh
+++ .scripts/waybar-battery-discharge.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+#---------------------------------------------------------------------
+# GET BATTERY INFORMATION
+#---------------------------------------------------------------------
+
 # Get voltage from the kernel and store it in mV
 voltage_now=$(( $(cat /sys/class/power_supply/BAT1/voltage_now) / 1000 ))
 
@@ -9,9 +13,16 @@ current_now=$(( $(cat /sys/class/power_supply/BAT1/current_now) / 1000 ))
 # Calculate power in uW
 power_now=$(( $voltage_now * $current_now ))
 
+# Get battery state from the kernel
+battery_status=$(cat /sys/class/power_supply/BAT1/status)
+
+#---------------------------------------------------------------------
+# STATUS-BASED CACHING
+#---------------------------------------------------------------------
+
 # The file to be used to store the max power as a cache (convenient way to track
 # a maximum for the given system
-POWER_MAX_CACHE="$HOME/.cache/.dotfiles/power_max"
+POWER_MAX_CACHE="$HOME/.cache/.dotfiles/power_max_${battery_status}"
 
 # Default power_max to power_now, but try to retreive it from a file
 power_max=$power_now
@@ -25,7 +36,12 @@ fi
 # TODO this requires the ~/.cache/.dotfiles directory to exist. Make it here or ensure it's made on install.
 if [ "$power_now" -ge "$power_max" ]; then
     echo $power_now > $POWER_MAX_CACHE
+    power_max=$power_now
 fi
+
+#---------------------------------------------------------------------
+# FORMATTING ARITHMETIC
+#---------------------------------------------------------------------
 
 # Do some funky math to allow printing decimal numbers using only integer variables
 power_now_whole=$(( $power_now / 1000000 ))
@@ -38,16 +54,38 @@ power_max_frac=$(( ( $power_max % 1000000 ) / 10000 ))
 # Dividing the divisor by 100 first gives an accurate percentage via pure integer math
 percentage=$(( $power_now / $(( $power_max / 100 )) ))
 
-# Determine the application of style classes for coloring the bar
+#---------------------------------------------------------------------
+# CONDITIONAL FORMATTING
+#---------------------------------------------------------------------
+
+# Conditionally choose a color for the bar and apply it with a span
+# Using this many CSS classes would majorly clutter the style sheet, but if a
+# span is used the clutter can stay contained in here
 if [ $percentage -le 25 ]; then
-    STYLE_CLASS="blue-soft"
+    if [ "$battery_status" = "Discharging" ]; then
+        STYLE_CLASS="discharging-low"
+    else
+        STYLE_CLASS="charging-low"
+    fi
 elif [ $percentage -ge 90 ]; then
-    STYLE_CLASS="red-soft"
-elif [ $percentage -ge 80 ]; then
-    STYLE_CLASS="yellow"
+    if [ "$battery_status" = "Discharging" ]; then
+        STYLE_CLASS="discharging-high"
+    else
+        STYLE_CLASS="charging-high"
+    fi
+elif [ $percentage -ge 75 ]; then
+    if [ "$battery_status" = "Discharging" ]; then
+        STYLE_CLASS="discharging-med"
+    else
+        STYLE_CLASS="charging-med"
+    fi
 else
     STYLE_CLASS=""
 fi
 
+#---------------------------------------------------------------------
+# PRINTF RETURN
+#---------------------------------------------------------------------
+
 # Return data to Waybar in JSON
-printf '{"tooltip": "%d.%dW of %d.%dW max", "percentage": %d, "class": "%s"}' $power_now_whole $power_now_frac $power_max_whole $power_max_frac $percentage $STYLE_CLASS
+printf "{\"tooltip\": \"%s %d.%dW of %d.%dW max\", \"percentage\": %d, \"class\": \"%s\"}" $battery_status $power_now_whole $power_now_frac $power_max_whole $power_max_frac $percentage $STYLE_CLASS
