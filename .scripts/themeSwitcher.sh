04adc46964b98c129361708a547ffa592b11083f
ambertia <95402631+ambertia@users.noreply.github.com>
2024-12-31 01:14:07 -0800

    ➖ Remove orphaned package gradience from themeswitcher

--- .scripts/themeSwitcher.sh
+++ .scripts/themeSwitcher.sh
@@ -14,10 +14,6 @@ cp $BACKGROUND_PATH $HOME/.backgrounds/current-wallpaper
 swww img $BACKGROUND_PATH
 # Generate palette
 wal -i $BACKGROUND_PATH --cols16 -n --contrast 2 -a 50
-# Import palette into Gradience
-gradience-cli import -p ~/.cache/wal/pywal.json
-# Apply Gradience pallete to GTK theme
-gradience-cli apply -n pywal
 # Run script to apply pywal theme to Mako notifications
 ~/.scripts/update-mako-theme.sh
 # Prevent duplicate waybar instances from modifications to hyprland.conf colors
