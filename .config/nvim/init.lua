c138756140c62af0fcd7a2526f2ff18d1e6a1d14
ambertia <95402631+ambertia@users.noreply.github.com>
2025-03-30 20:57:17 -0700

    🚸 Add neovim keybinding to search for todo-comments

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -200,6 +200,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Open the search for Todos
+vim.keymap.set('n', '<leader>st', ':TodoQuickFix<CR>', { desc = '[S]earch for [T]ODO comments' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
